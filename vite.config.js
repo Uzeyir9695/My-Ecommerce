f3c2dfb5aa205351983f8b6763f1f976741eb92b
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2023-06-08 22:16:58 +0400

    disable the transformation of asset URLs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
-
 export default defineConfig({
     plugins: [
+
         laravel({
             input: [
                 'resources/css/app.css',
@@ -10,5 +11,19 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+
     ],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js'
+        }
+    }
 });
