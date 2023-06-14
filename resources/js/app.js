7dd7ec31e82f4d5c1777c96f54737fef4b8868ab
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2023-06-14 23:50:21 +0400

    register components and fontawesome icons

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,21 @@
 import './bootstrap';
-import { createApp} from "vue";
-import ProductIndex from './products/ProductIndex.vue'
+import { createApp } from 'vue';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import store from './store';
+import Index from './products/Index.vue';
+import CartList from './cart/CartList.vue';
+import Wishlist from './wishlist/Wishlist.vue';
 
-createApp({
+const app = createApp({
     components: {
-        ProductIndex
+        Index,
+        CartList,
+        Wishlist
     }
-}).mount('#app');
-
+});
+// Register FontAwesomeIcon component globally
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(store);
+app.mount('#app');
