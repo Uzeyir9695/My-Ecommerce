e6fb333e4e27bcbbe0d012e9b2ff2883f13245de
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2023-06-14 23:54:32 +0400

    create product index component and add filter functionality according to the attribute_values

--- resources/js/products/Index.vue
+++ resources/js/products/Index.vue
@@ -1,9 +1,6 @@
 <template>
-
     <!-- BEGIN: Content-->
     <div class="app-content content ecommerce-application">
-        <div class="content-overlay"></div>
-        <div class="header-navbar-shadow"></div>
         <div class="content-wrapper container-xxl p-0">
             <div class="content-header row">
                 <div class="content-header-left col-md-9 col-12 mb-2">
@@ -88,11 +85,15 @@
                     <!-- E-commerce Search Bar Ends -->
 
                     <!-- E-commerce Products Starts -->
-                    <section id="ecommerce-products" class="grid-view">
-                        <div class="card ecommerce-card" v-for="product in products.data">
+                    <section id="ecommerce-products" class="grid-view" v-if="products && products.length > 0">
+<!--                        Temporary comment out spinner-->
+<!--                        <div v-if="contentLoading" class="spinner-grow text-primary" id="content-loading"> &lt;!&ndash; Add your spinner HTML or CSS class here &ndash;&gt;-->
+<!--                            &lt;!&ndash; Spinner HTML or CSS &ndash;&gt;-->
+<!--                        </div>-->
+                        <div class="card ecommerce-card" v-for="product in products">
                             <div class="item-img text-center">
                                 <a :href="routes.productShow+'/'+product.id">
-                                    <img class="img-fluid card-img-top" :src="product.media[0].original_url" style="width: 350px; height: 250px;"  alt="img-placeholder" /></a>
+                                    <img class="img-fluid card-img-top" :src="product.media[0].original_url" style="width: 500px; height: 250px;"  alt="img-placeholder" /></a>
                             </div>
                             <div class="card-body">
                                 <div class="item-wrapper">
@@ -105,7 +106,8 @@
                                             <li class="ratings-list-item"><i data-feather="star" class="unfilled-star"></i></li>
                                         </ul>
                                     </div>
-                                    <div>
+                                    <div class="d-flex justify-content-center">
+                                        <h6 v-if="product.discount>0"><del>$<span class="wishlist-price mr-1">{{ product.price }}</span></del> </h6>
                                         <h6 class="item-price">${{ price(product.price, product.discount) }}</h6>
                                     </div>
                                 </div>
@@ -116,51 +118,38 @@
                                 <p class="card-text item-description">{{ product.description }}</p>
                             </div>
                             <div class="item-options">
-                                <div class="item-wrapper">
-                                    <div class="item-cost">
-                                        <h4 class="item-price">${{ price(product.price, product.discount) }}</h4>
-                                    </div>
-                                </div>
-                                <a href="javascript:void(0)" class="btn btn-light btn-wishlist">
-                                    <form action="" method="POST" id="add-item-to-wishlist">
-                                        <input type="hidden" name="id" :value="product.id">
-                                        <i data-feather="heart"></i>
-                                        <span>Wishlist</span>
-                                    </form>
+                                <a href="javascript:void(0)" @click="addToWishlist(product.id)" class="btn btn-light btn-wishlist">
+                                    <i data-feather="heart"></i>
+                                    <span>Wishlist</span>
                                 </a>
-                                <a :href="cartLink" @click="addToCart(product.id)" class="btn btn-primary btn-cart">
-                                    <form :id="'myForm-'+product.id">
-                                        <input type="hidden" name="id" :value="product.id">
-                                        <input type="hidden" name="price" :value="price(product.price, product.discount)">
-                                        <input type="hidden" name="discount" :value="product.discount">
-                                        <i data-feather="shopping-cart"></i>
-                                        <span class="add-to-cart">Add to cart</span>
-                                    </form>
+                                <a :href="cartLink" @click="addToCart(product)" class="btn btn-primary btn-cart">
+                                    <i data-feather="shopping-cart"></i>
+                                    <span class="add-to-cart">Add to cart</span>
                                 </a>
                             </div>
                         </div>
-<!--                        display if product not found-->
-                        <div class="row col-12"  v-if="products.data && products.data.length === 0">
-                            <h3 class="float-right mt-3 text-danger">Product not found!</h3>
-                        </div>
+
                     </section>
                     <!-- E-commerce Products Ends -->
-
-                    <!-- E-commerce Pagination Starts -->
-                    <section id="ecommerce-pagination" v-if="products.data.length > 21">
+                    <!--  display if product not found-->
+                    <div class="row col-12"  v-if="!products && products.length === 0">
+                        <h3 class="float-right mt-3 text-danger">Product not found!</h3>
+                    </div>
+                    <!-- E-commerce Pagination Products -->
+                    <section id="ecommerce-pagination" v-if="paginateProducts && paginateProducts.total > 21">
                         <div class="row d-flex justify-content-center">
-                            <nav >
+                            <nav>
                                 <ul class="pagination">
-                                    <li class="page-item" :class="{ 'disabled': currentPage === 1 }">
-                                        <a class="page-link" href="#" @click.prevent="paginate(currentPage - 1)" aria-label="Previous">
+                                    <li class="page-item" :class="{ 'disabled': paginateProducts.current_page === 1 }">
+                                        <a class="page-link" href="#" @click.prevent="paginate(paginateProducts.current_page - 1)" aria-label="Previous">
                                             <span aria-hidden="true">&laquo;</span>
                                         </a>
                                     </li>
-                                    <li class="page-item" :class="{ 'active': page === currentPage }" v-for="page in lastPage" :key="page">
-                                        <a class="page-link" :href="routes.paginateRoute+'?page='+page" >{{ page }}</a>
+                                    <li class="page-item" :class="{ 'active': page === paginateProducts.current_page }" v-for="page in paginateProducts.last_page" :key="page">
+                                        <a class="page-link" href="#" @click.prevent="paginate(page)">{{ page }}</a>
                                     </li>
-                                    <li class="page-item" :class="{ 'disabled': currentPage === lastPage }">
-                                        <a class="page-link" href="#" @click.prevent="paginate(currentPage + 1)" aria-label="Next">
+                                    <li class="page-item" :class="{ 'disabled': paginateProducts.current_page === paginateProducts.last_page }">
+                                        <a class="page-link" href="#" @click.prevent="paginate(paginateProducts.current_page + 1)" aria-label="Next">
                                             <span aria-hidden="true">&raquo;</span>
                                         </a>
                                     </li>
@@ -189,7 +178,12 @@
                                         <li v-for="value in values">
                                             <form action="" method="post">
                                                 <div class="custom-control custom-checkbox ">
-                                                    <input type="checkbox" class="custom-control-input" :id="`washer-${value.id}`" name="household">
+                                                    <input type="checkbox" class="custom-control-input  attribute-checkbox" :id="`washer-${value.id}`"
+                                                           :name="name"
+                                                           :value="value.value"
+                                                           :checked="isSelected(name, value.value)"
+                                                           @change="handleCheckboxChange(name, value.value)"
+                                                    >
                                                     <label class="custom-control-label" :for="`washer-${value.id}`">{{ value.value }}</label>
                                                 </div>
                                             </form>
@@ -258,40 +252,33 @@
 
                                 <!-- Clear Filters Starts -->
                                 <div id="clear-filters">
-                                    <button type="button" class="btn btn-block btn-primary">Clear All Filters</button>
+                                    <button @click="clearFilters" type="button" class="btn btn-block btn-primary">Clear All Filters</button>
                                 </div>
                                 <!-- Clear Filters Ends -->
                             </div>
                         </div>
                     </div>
                     <!-- Ecommerce Sidebar Ends -->
-
                 </div>
             </div>
         </div>
-
     </div>
     <!-- END: Content-->
 </template>
 
+
+
 <script>
+import { mapGetters } from 'vuex'
 export default {
     props: {
-        attributes: {
-            type: Object,
-            required: true
-        },
-        products: {
-            type: Object,
-            required: true
-        },
         subCatRoute: {
             type: String
         },
         productShowRoute: {
             type: String
         },
-        wishlistRoute: {
+        addWishlistRoute: {
             type: String
         },
         breadcrumb: {
@@ -303,55 +290,136 @@ export default {
             routes: {
                 paginateRoute: this.subCatRoute,
                 productShow: this.productShowRoute,
-                wishlistCreate: this.wishlistRoute,
+                wishlistCreate: this.addWishlistRoute,
             },
             cartLink: '#',
-            addedToCart: true,
+            selectedFilters: {},
         }
     },
+
+    created() {
+        this.$store.dispatch('product/fetchProducts', this.routes.paginateRoute);
+    },
+
     computed: {
-        currentPage() {
-            return this.products.current_page;
-        },
-        lastPage() {
-            return this.products.last_page;
-        }
+        ...mapGetters({
+            products: 'product/products',
+            attributes: 'product/attributes',
+            contentLoading: 'product/contentLoading',
+            paginateProducts: 'product/paginateProducts',
+        })
     },
-    mounted() {
-       this.getNavbarCarts();
-    },
+
     methods: {
-        addToCart(id) {
-            // Get the form element
-            const form = document.getElementById('myForm-'+id);
-            // Retrieve input field values from the form
-            const product_id = form.elements.id.value;
-            const price = form.elements.price.value;
-            const discount = form.elements.discount.value;
+        handleCheckboxChange(attributeName, attributeValue) {
+            if (!this.selectedFilters.hasOwnProperty(attributeName)) {
+                this.selectedFilters[attributeName] = [];
+            }
+            const attributeFilters = this.selectedFilters[attributeName];
+            const index = attributeFilters.indexOf(attributeValue);
+            if (index > -1) {
+                attributeFilters.splice(index, 1);
+            } else {
+                attributeFilters.push(attributeValue);
+            }
 
-            // AJAX request using Axios
-            axios.post('/carts', { product_id, price, discount })
+            // // Update localStorage with the current checkbox state
+            // const key = `${attributeName}_${attributeValue}`;
+            // const isChecked = attributeFilters.includes(attributeValue);
+            // // Store the checkbox state in localStorage
+            // localStorage.setItem(key, JSON.stringify(isChecked));
+
+            // Update the selectedFilters object
+            this.updateSelectedFilters();
+
+            // Send AJAX request or perform any necessary operations with the selected filters
+            const payload = { paginateRoute: this.routes.paginateRoute, filters: this.selectedFilters };
+            this.$store.dispatch('product/fetchProducts', payload);
+        },
+
+        updateSelectedFilters() {
+            // Create a new object to store the updated selected filters
+            const updatedSelectedFilters = {};
+            // Loop through the selectedFilters object and remove unchecked attribute names and their values
+            for (const attributeName in this.selectedFilters) {
+                const attributeValues = this.selectedFilters[attributeName];
+                if (attributeValues.length > 0) {
+                    updatedSelectedFilters[attributeName] = attributeValues;
+                }
+            }
+            // Assign the updated object to selectedFilters
+            this.selectedFilters = updatedSelectedFilters;
+        },
+
+        // retrieveCheckboxValues() {
+        //     for (const [attributeName, values] of Object.entries(this.attributes)) {
+        //         for (const value of values) {
+        //             const key = `${attributeName}_${value.value}`;
+        //             const isChecked = localStorage.getItem(key);
+        //             if (isChecked !== null && JSON.parse(isChecked)) {
+        //                 if (!this.selectedFilters.hasOwnProperty(attributeName)) {
+        //                     this.selectedFilters[attributeName] = [];
+        //                 }
+        //                 this.selectedFilters[attributeName].push(value.value);
+        //             }
+        //         }
+        //     }
+        // },
+
+        isSelected(attributeName, attributeValue) {
+            return (
+                this.selectedFilters.hasOwnProperty(attributeName) &&
+                this.selectedFilters[attributeName].includes(attributeValue)
+            );
+        },
+
+        clearFilters() {
+            // Clear the selectedFilters object and localStorage
+            for (const attributeName in this.selectedFilters) {
+                for (const attributeValue of this.selectedFilters[attributeName]) {
+                    const key = `${attributeName}_${attributeValue}`;
+                    localStorage.removeItem(key);
+                }
+            }
+            this.selectedFilters = {};
+
+            // Send AJAX request or perform any necessary operations to clear the filters
+            const payload = { paginateRoute: this.routes.paginateRoute, filters: this.selectedFilters };
+            this.$store.dispatch('product/fetchProducts', payload);
+        },
+
+        async paginate(page) {
+            // Call your Vuex action to fetch data for the specified page
+            await this.$store.dispatch('product/fetchProducts', this.routes.paginateRoute+'?page='+page);
+        },
+
+        addToCart(product) {
+            const item = {
+                product_id: product.id,
+                price: this.price(product.price, product.discount),
+                discount: product.discount
+            }
+            this.$store.dispatch('cart/addToCart', item)
                 .then(response => {
-                 this.cartLink = this.routes.productShow+'/'+id;
-                    console.log('Added cart');
+                    this.cartLink = this.routes.productShow+'/'+product.id;
                 })
                 .catch(error => {
-                    // Handle errors
+                    console.log(error);
                 });
         },
 
-        getNavbarCarts() {
-            axios.get('/navbar-carts')
+        addToWishlist(product_id) {
+            this.$store.dispatch('wishlist/addToWishlist', product_id)
                 .then(response => {
-                    console.log(response)
-                    // Handle the response
                 })
                 .catch(error => {
-                    // Handle errors
+                    console.log(error);
                 });
         },
+
+        // calculate product's discount
         price(price, discount=0){
-                return price - (price*discount/100);
+            return price - (price*discount/100);
         },
     }
 }
