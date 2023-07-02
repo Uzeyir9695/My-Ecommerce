909d5090707ce2318f3877aa04ee52800bfb8ea0
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2023-07-03 01:54:53 +0400

    add discount column in validation

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use App\Models\Subcategory;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use function PHPUnit\Framework\at;
 
@@ -27,27 +28,24 @@ class ProductRequest extends FormRequest
      */
     public function rules()
     {
-        $inputs =implode(',', ['store_id', 'category_id', 'subcategory_id', 'description', 'quantity', 'images']);
-
+//        $inputs =implode(',', ['category_id', 'subcategory_id', 'description', 'quantity', 'images']);
         return [
-            'store_id', 'required|integer',
             'category_id' => 'required|integer',
             'subcategory_id' => 'required|integer',
             'name' => 'bail|required|min:3|max:100',
             'price' => 'bail|required|max:255',
             'description' => 'required|min: 5|max: 500',
             'quantity' => 'required|max:10000',
+            'discount' => 'nullable|integer|max:100',
             'images' => 'required',
             'images.*' => 'bail|required|image|mimes:jpg,png,jpeg,gif,svg|max:2048|dimensions:min_width=100,min_height=100,max_width=2000,max_height=2000',
-            'agreed' => 'required_with_all:'.$inputs.','.(request('subcategory_id') ? $this->dynamicData()[1] : ''),
-
-            ] + (request()->filled('subcategory_id') ? $this->dynamicData()[0] : []);
+            'agreed' => 'required|in:true',
+            ] + (request()->filled('subcategory_id') ? $this->dynamicData()[1] : []);
     }
 
     public function messages()
     {
         return [
-            'store_id.required' => 'The store is required!',
             'category_id.required' => 'The category is required!',
             'subcategory_id.required' => 'The subcategory is required!',
             'agreed.required_with_all' => 'Almost done! Just agree to our terms and conditions and continue.',
@@ -57,19 +55,18 @@ class ProductRequest extends FormRequest
     public function dynamicData()
     {
         if (request()->has('subcategory_id')){
-            $assocArray = [];
-            $inputNames = [];
-            $attributes = Subcategory::find(request('hidden_subcategory_id'))->attributes()->get()->groupBy(function ($data) {
-                return $data->name;
-            });
-            foreach ($attributes as $name => $values) {
-                $snakeCaseAttribute = Str::replace(' ', '_', $name);
-                $lowerCaseAttribute = strtolower($snakeCaseAttribute);
-                $assocArray[$lowerCaseAttribute] = 'required_with:subcategory_id,*';
-                $inputNames[] = $lowerCaseAttribute;
-                $attributesAsString =  implode(',', $inputNames);
-            }
-            return [$assocArray, $attributesAsString];
+                $json = request('attributes');
+                $dataArray = json_decode($json, true);
+
+            $stringKeys = implode(',', array_map(function ($key) {
+                return Str::snake(strtolower($key));
+            }, array_keys($dataArray)));
+
+            $assocArray = collect($dataArray)->mapWithKeys(function ($value, $key) {
+                return [Str::snake(strtolower($key)) => 'required_with:subcategory_id,*'];
+            })->all();
+
+            return [$stringKeys, $assocArray];
         }
     }
 }
