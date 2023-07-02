027561b06eebdae0cef55f09ef3012f740f50c81
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2023-07-03 02:10:33 +0400

    dynamically pass parameters to subcategories.index route

--- resources/views/ecommerce/index.blade.php
+++ resources/views/ecommerce/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
         <Index
             breadcrumb="{{ Str::title(request()->segment(4)) }}"
-            :sub-cat-route="'{{ route('subcategories.index', [request()->segment(3), request()->segment(4)]) }}'"
+            :sub-cat-route="'{{ route('subcategories.products', [request()->segment(3), request()->segment(4)]) }}'"
             :product-show-route="'{{ route('products.show', "") }}'"
             :add-wishlist-route="'{{ route('wishlists.store') }}'"
         ></Index>
