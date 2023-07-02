4a116a22f7d76d53d0cb6f51d45873fe823a8498
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2023-07-03 02:17:00 +0400

    create success page for payment

---
+++ resources/views/success.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.master')
+@section('content')
+    <!-- BEGIN: Content-->
+    <div class="app-content content ecommerce-application">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper container-xxl p-0">
+            <div class="d-flex flex-column align-items-center">
+                <h1 class="d-block text-success mb-1">Payment was successfull!</h1>
+                <h3 class="d-block mb-1">Thanks for your purchase!</h3>
+                <h4 class="d-block">Continue <a href="{{ route('subcategories.index', ['1', 'washing-machines']) }}"> Shopping!</a></h4>
+            </div>
+        </div>
+    </div>
+@endsection
