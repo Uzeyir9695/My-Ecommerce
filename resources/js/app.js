dfba96ac8b672d231a9356b135b9851e86f3da0f
Uzeyir9695 <uzeira.shabanovi@gmail.com>
2023-07-03 02:15:38 +0400

    add some components and sweetalert and fontawesome packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,21 +1,38 @@
 import './bootstrap';
 import { createApp } from 'vue';
+import store from './store';
+// Packages
 import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import store from './store';
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+// Components
 import Index from './products/Index.vue';
+import ProductCreate from './products/Create.vue';
+import ProductEdit from './products/Edit.vue';
+import ShopCreate from './shops/Create.vue';
+import ShopEdit from './shops/Edit.vue';
 import CartList from './cart/CartList.vue';
+import Checkout from './cart/Checkout.vue';
 import Wishlist from './wishlist/Wishlist.vue';
 
 const app = createApp({
     components: {
         Index,
+        ProductCreate,
+        ProductEdit,
         CartList,
-        Wishlist
+        Wishlist,
+        Checkout,
+        ShopCreate,
+        ShopEdit
     }
 });
 // Register FontAwesomeIcon component globally
-app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(store);
+app.use(VueSweetalert2);
+app.component('font-awesome-icon', FontAwesomeIcon);
+window.Swal =  app.config.globalProperties.$swal;
 app.mount('#app');
